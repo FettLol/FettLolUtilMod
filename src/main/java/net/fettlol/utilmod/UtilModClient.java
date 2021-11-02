4b623e5bcbe86ff876930f658af30209baccd9b0
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-11-02 23:54:01 +0100

    use new EntityRendererRegistry

--- src/main/java/net/fettlol/utilmod/UtilModClient.java
+++ src/main/java/net/fettlol/utilmod/UtilModClient.java
@@ -1,7 +1,7 @@
 package net.fettlol.utilmod;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fettlol.utilmod.init.FettlolEntities;
 import net.fettlol.utilmod.entities.render.WanderingHeadHunterRender;
 
@@ -14,7 +14,7 @@ public class UtilModClient implements ClientModInitializer {
 
     private void initializeEntities() {
         // Register the Wandering Headhunter in the client's Entity Renderer Registry.
-        EntityRendererRegistry.INSTANCE.register(
+        EntityRendererRegistry.register(
             FettlolEntities.WANDERING_HEAD_HUNTER,
             WanderingHeadHunterRender::new
         );
