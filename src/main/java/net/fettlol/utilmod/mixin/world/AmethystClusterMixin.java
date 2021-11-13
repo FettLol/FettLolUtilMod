af6ed78ae87eba686359f4e4fc4ea655c090166a
Breki Tomasson <breki.tomasson@gmail.com>
2021-11-13 01:21:13 +0100

    renamed helper methods

--- src/main/java/net/fettlol/utilmod/mixin/world/AmethystClusterMixin.java
+++ src/main/java/net/fettlol/utilmod/mixin/world/AmethystClusterMixin.java
@@ -1,6 +1,6 @@
 package net.fettlol.utilmod.mixin.world;
 
-import net.fettlol.lib.list.Tags;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.AmethystBlock;
 import net.minecraft.block.AmethystClusterBlock;
 import net.minecraft.block.BlockState;
@@ -24,30 +24,36 @@ public abstract class AmethystClusterMixin extends AmethystBlock {
 
     @Override
     public void afterBreak(World world, PlayerEntity playerEntity, BlockPos blockPos, BlockState blockState, BlockEntity blockEntity, ItemStack itemStack) {
-        // Run whatever is in the parent classes' afterBreak first.
         super.afterBreak(world, playerEntity, blockPos, blockState, blockEntity, itemStack);
 
-        if (passesCriteria(world, blockState, itemStack)) {
-            this.dropExperience((ServerWorld) world, blockPos, getExpDropped(world, itemStack));
+        if (blockShouldDropExperienceWhenBroken(world, blockState, itemStack)) {
+            this.dropExperience((ServerWorld) world, blockPos, getTotalExperienceToDrop(world, itemStack));
         }
     }
 
-    private boolean passesCriteria(World world, BlockState blockState, ItemStack itemStack) {
-        // Run on the server world, ensure the block is an Amethyst Cluster, and that it's not broken with Silk Touch
+    private boolean blockShouldDropExperienceWhenBroken(World world, BlockState blockState, ItemStack itemStack) {
         return !world.isClient
             && blockState.isOf(Blocks.AMETHYST_CLUSTER)
             && EnchantmentHelper.getLevel(Enchantments.SILK_TOUCH, itemStack) == 0;
     }
 
-    private int getExpDropped(World world, ItemStack itemStack) {
+    private int getTotalExperienceToDrop(World world, ItemStack itemStack) {
         return heldItemIsPickaxe(itemStack)
             ? 2 + world.random.nextInt(5)
             : 1 + world.random.nextInt(3);
     }
 
+    /**
+     * This method should probably be refactored into `fettlol-lib` and made part of a larger
+     * collection of methods that take an ItemStack as their argument and return a boolean.
+     * In this case, we're checking if an item is a Pickaxe in two different ways, since not
+     * all pickaxes added via mods are in minecraft:cluster_max_harvestables, but they should
+     * at least be in fabric:pickaxes. All things considered, there should probably be a check
+     * to see if the item is an instance of PickaxeItem, too.
+     */
     private boolean heldItemIsPickaxe(ItemStack itemStack) {
-        // Since not all modded pickaxes are in minecraft:cluster_max_harvestables.
-        return itemStack.isIn(ItemTags.CLUSTER_MAX_HARVESTABLES) || itemStack.isIn(Tags.FABRIC_PICKAXES);
+        return itemStack.isIn(ItemTags.CLUSTER_MAX_HARVESTABLES)
+            || itemStack.isIn(FabricToolTags.PICKAXES);
     }
 
 }
