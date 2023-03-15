9d46d322b85f0b4b9dc31e072c1228212d69ef56
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-03-15 21:20:09 +0100

    fix experimental settings warning

--- src/main/java/net/fettlol/utilmod/mixin/client/IntegratedServerLoaderMixin.java
+++ src/main/java/net/fettlol/utilmod/mixin/client/IntegratedServerLoaderMixin.java
@@ -2,7 +2,7 @@ package net.fettlol.utilmod.mixin.client;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.server.integrated.IntegratedServerLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -11,13 +11,13 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
  * Disable the experimental settings warning.
  */
 @Environment(EnvType.CLIENT)
-@Mixin(MinecraftClient.class)
-public abstract class MinecraftClientMixin {
+@Mixin(IntegratedServerLoader.class)
+public abstract class IntegratedServerLoaderMixin {
 
     @ModifyVariable(
-        method = "startIntegratedServer(Ljava/lang/String;Ljava/util/function/Function;Ljava/util/function/Function;ZLnet/minecraft/client/MinecraftClient$WorldLoadAction;)V",
+        method = "start(Lnet/minecraft/client/gui/screen/Screen;Ljava/lang/String;ZZ)V",
         at = @At("STORE"),
-        ordinal = 2 // includes safeMode parameter
+        ordinal = 3 // includes safeMode and canShowBackupPrompt parameter
     )
     private boolean skipExperimentalSettingsWarning(boolean isExperimentalLifecycle) {
         return false;
