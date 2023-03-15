2bd3d23051d97b0cd58f9ac052046c53e37e185c
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-03-15 21:13:30 +0100

    fix tag loader mixin

--- src/main/java/net/fettlol/utilmod/mixin/core/TagGroupLoaderMixin.java
+++ src/main/java/net/fettlol/utilmod/mixin/core/TagGroupLoaderMixin.java
@@ -2,8 +2,9 @@ package net.fettlol.utilmod.mixin.core;
 
 import net.fettlol.utilmod.registry.TagApi;
 import net.minecraft.resource.ResourceManager;
-import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagEntry;
 import net.minecraft.tag.TagGroupLoader;
+import net.minecraft.tag.TagGroupLoader.TrackedEntry;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,8 +12,9 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -23,31 +25,20 @@ public class TagGroupLoaderMixin {
     @Shadow
     private String dataType;
 
-    @Inject(method = "loadTags", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILHARD)
-    public void loadTags(ResourceManager rm, CallbackInfoReturnable<Map<Identifier, Tag.Builder>> ci, Map<Identifier, Tag.Builder> map) {
-        apply(dataType, map);
-    }
+    @Inject(method = "loadTags", at = @At("RETURN"))
+    public void loadTags(ResourceManager rm, CallbackInfoReturnable<Map<Identifier, List<TrackedEntry>>> ci) {
+        Map<Identifier, List<TrackedEntry>> map = ci.getReturnValue();
 
-    private static void apply(String entry, Map<Identifier, Tag.Builder> tagsMap) {
-        Map<Identifier, Set<Identifier>> fettlolTags = null;
-
-        if (entry.equals("tags/items")) {
-            fettlolTags = TagApi.buildItemTags();
-        }
-
-        if (fettlolTags != null) {
-            fettlolTags.forEach((id, ids) ->
-                tagsMap.compute(id, (k, v) ->
-                    v == null
-                        ? apply(Tag.Builder.create(), ids)
-                        : apply(v, ids)));
+        if (dataType.equals("tags/items")) {
+            TagApi.buildItemTags().forEach((tagId, itemIds) ->
+                map.compute(tagId, (k, entryList) ->
+                    addIdsToEntryList(entryList == null ? new ArrayList<>(itemIds.size()) : entryList, itemIds)));
         }
     }
 
-    private static Tag.Builder apply(Tag.Builder builder, Set<Identifier> ids) {
-        ids.forEach((value) -> builder.add(value, "Fettlol Library"));
-        return builder;
+    private static List<TrackedEntry> addIdsToEntryList(List<TrackedEntry> entryList, Set<Identifier> ids) {
+        ids.forEach((value) -> entryList.add(new TrackedEntry(TagEntry.create(value), "Fettlol Library")));
+        return entryList;
     }
-
 
 }
