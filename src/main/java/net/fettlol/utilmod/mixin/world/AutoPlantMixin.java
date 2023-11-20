e157af653870bfbadd5cabb91eb1a81b78774c36
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-11-20 18:17:41 +0100

    planting a log won't do no good

--- src/main/java/net/fettlol/utilmod/mixin/world/AutoPlantMixin.java
+++ src/main/java/net/fettlol/utilmod/mixin/world/AutoPlantMixin.java
@@ -40,6 +40,8 @@ public abstract class AutoPlantMixin extends Entity {
             if (player != null) {
                 fettlol$autoPlant = getPlayerConfig(player).autoPlant;
             }
+        } else {
+            fettlol$autoPlant = false;
         }
     }
 
